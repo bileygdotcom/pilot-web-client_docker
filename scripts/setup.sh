acdfabc4e729291a0af5fbc3c49c1a08ceb025c5
bileyg <bileyg@yandex.ru>
2024-06-11 12:48:05 +0300

    remove settings builder

--- scripts/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-printf "  \"PilotServer\": {\n    \"Url\": \"$SERVER\",\n    \"Database\": \"$DATABASE\",\n    \"LicenseCode\": \"$LICENSE\",\n  },\n\n" > appsettings_p2
-cat appsettings_p1 appsettings_p2 appsettings_p3 > appsettings.json
